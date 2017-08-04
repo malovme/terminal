287df0b5bd96c78d8740608846f0b54bacea6375
Michael Malov <mmeemail@gmail.com>
2017-08-04 16:15:29 +0300

    base logic

--- README.md
+++ README.md
@@ -20,6 +20,28 @@ Or install it yourself as:
 
 ## Usage
 
+```ruby
+# Create Terminal instance
+terminal = Terminal::Terminal.new
+# Set pricing providing hash with amount key and cost value for each product code
+terminal.set_pricing({A: {1 => 2, 4 => 7},
+                      B: {1 => 12},
+                      C: {1 => 1.25, 6 => 6},
+                      D: {1 => 0.15}})
+terminal.scan('A')
+terminal.scan('D')
+terminal.total
+# => 2.15
+
+# if you nedd to count next order with same pricing
+terminal.next_order
+
+# You able to scan all products at once
+terminal.scan('ABCDABAA')
+terminal.total
+#
+```
+
 TODO: Write usage instructions here
 
 
--- lib/terminal.rb
+++ lib/terminal.rb
@@ -1,5 +1,66 @@
 require "terminal/version"
 
 module Terminal
-  # Your code goes here...
+  class Terminal
+    attr_reader :pricing
+    attr_reader :products
+
+    def initialize(pricing = {}, products = [])
+      @pricing = pricing
+      @products = products
+    end
+
+    # set pricing with sorting by product amount
+    def set_pricing pricing
+      pricing.each do |code, prices|
+        @pricing[code.to_s] = {}
+        prices.keys.sort.each do |k|
+        @pricing[code.to_s][k] = prices[k]
+        end
+      end
+    end
+
+    def scan product_codes
+      product_codes.split("").each do |code|
+        @products.push(code)
+      end
+    end
+
+    def total
+      products_total = get_products_total
+      price = 0.0
+      products_total.each do |product, amount|
+        get_product_by_packs(product, amount).each do |pack|
+          price += @pricing[product][pack]
+        end
+      end
+      "$ #{'%.2f' % price}"
+    end
+
+    def next_order
+      @products = []
+    end
+
+private
+
+    def get_products_total
+      products_total = Hash.new(0)
+      @products.each do |code|
+        products_total[code] += 1
+      end
+      products_total
+    end
+
+    def get_product_by_packs(product, amount)
+      packs = []
+      if @pricing[product].keys.include?(amount)
+        packs.push(amount)
+      else
+        pack = @pricing[product].keys.select { |pack| pack < amount }.max
+        packs.push(pack)
+        packs + get_product_by_packs(product, amount - pack)
+      end
+    end
+
+  end
 end
